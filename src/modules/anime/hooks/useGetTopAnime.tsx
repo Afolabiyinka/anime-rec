12fdc5ea2f3f8ebc64303638498fe1acaff2a39f
Afolabi Olayinka <afolabiyinka738@gmail.com>
2026-06-10 08:34:33 +0100

    Update useGetTopAnime.tsx

--- src/modules/anime/hooks/useGetTopAnime.tsx
+++ src/modules/anime/hooks/useGetTopAnime.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getTopAnimes } from "../api/anime.request";
 
 export const useGetTopAnimes = () => {
-  const { data, error } = useQuery({
+  const { data,} = useQuery({
     queryKey: ["top-animes"],
     queryFn: getTopAnimes,
   });
